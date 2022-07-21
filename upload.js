b2b2c561cad4a6ff3a05134f7c9f48bb41399fea
agikedwin <agikedwin@gmail.com>
2022-07-21 17:57:26 +0300

    lambda upload files to s3

---
+++ upload.js
@@ -0,0 +1,44 @@
+const AWS  = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
+module.exports.hander = async event => {
+    console.log(event);
+
+    const response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Successfully uploaded the file to S3 bucket"
+        })
+    }
+
+    try{
+        const parseBody = JSON.parse(event.body);
+        const base64File = parsedBody.file;
+        console.log("recieving image file ______________")
+        //const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/,""), "base64");
+        const decodedFile = Buffer.from(base64File);
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: `images/${new Date().toISOString()}.*`,
+            Body: decodedFile,
+            ContentType: "image/*"
+        };
+
+        const uploadResult = await s3.upload(params).promise();
+        response.body = JSON.stringify({message: "Successfully uploaded the file to S3 bucket", uploadResult});
+
+
+    }catch(err){
+        console.log(err);
+        response.body = JSON.stringify({
+            message: "File failed to upload ",
+            errorMessage: err
+        });
+        response.statusCode =500;
+
+    }
+
+}
